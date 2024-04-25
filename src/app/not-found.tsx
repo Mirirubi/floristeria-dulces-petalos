88454c1197a5b072e05d5725d41ebcef9dd9bbcd
Mirirubi <mirirubi6@gmail.com>
2024-04-25 20:34:26 +0200

    Add_image_to_mot_found_page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,27 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Oh no!</h2>
-      <p>La hemos encontrado la página!</p>
-      Mejor <Link href="/">volvamos a la página principal!</Link>
+    <div className="flex flex-col px-auto items-center">
+      <div className="italic p-8">
+        <div>¿Cómo he terminado aquí?</div>
+      </div>
+      <Image
+        className="levitating-image "
+        src="/lost.png"
+        alt="MR"
+        width={300}
+        height={300}
+      />
+      <div className="italic p-8">
+        <div>
+          Será mejor que vuelva
+          <Link className="underline mx-2" href="/">
+            a la página principal!
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
